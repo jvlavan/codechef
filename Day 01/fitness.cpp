4d3a20ce934c5e6dcb7a1a6259fd049a896f7884
Lavan JV <70334120+jvlavan@users.noreply.github.com>
2023-07-25 18:36:38 +0530

    Create fitness.cpp

---
+++ Day 01/fitness.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a;
+	cin>>a;
+	int b;
+	while(a--){
+	    cin>>b;
+	    cout<<b*10<<endl;
+	
+	    
+	}
+	return 0;
+}
