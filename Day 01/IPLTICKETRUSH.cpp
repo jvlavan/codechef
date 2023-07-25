b5e64e5c03e7d026b7b524af3cd990e569c8b3fa
Lavan JV <70334120+jvlavan@users.noreply.github.com>
2023-07-25 18:58:02 +0530

    Create IPLTICKETRUSH.cpp

---
+++ Day 01/IPLTICKETRUSH.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a;
+	cin>>a;
+	int b,c;
+	while(a--){
+	    cin>>b>>c;
+	    if(b<c)
+	    cout<<"0"<<endl;
+	    else
+	    cout<<b-c<<endl;
+	
+	    
+	}
+	return 0;
+}
