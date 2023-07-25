934a50c6e61dca5ad24486e040a7d39509df315a
Lavan JV <70334120+jvlavan@users.noreply.github.com>
2023-07-25 18:33:17 +0530

    Create Masterfinals.cpp

---
+++ Day 01/Masterfinals.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a;
+	cin>>a;
+	int b;
+	while(a--){
+	    cin>>b;
+	    if(b<=10)
+	    cout<<"YES"<<endl;
+	    else
+	    cout<<"NO"<<endl;
+	
+	    
+	}
+	return 0;
+}
