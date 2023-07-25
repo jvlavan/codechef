9acbaa094200d4ac09ae51bd96772fda8c12ce16
Lavan JV <70334120+jvlavan@users.noreply.github.com>
2023-07-25 18:43:36 +0530

    Create Burgers.cpp

---
+++ Day 01/Burgers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a;
+	cin>>a;
+	int b,c;
+	int t;
+	while(a--){
+	    cin>>b>>c;
+	    t=b<c?b:c;
+	    cout<<t<<endl;
+	
+	    
+	}
+	return 0;
+}
