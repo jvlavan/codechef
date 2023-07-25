22528adf1f4f407b676b509f3e47e5352a7daade
Lavan JV <70334120+jvlavan@users.noreply.github.com>
2023-07-25 19:32:28 +0530

    Create practicemakesusperfect.cpp

---
+++ Day 01/practicemakesusperfect.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int c=0;
+	int t;
+	for(int i=0;i<4;i++){
+	    cin>>t;
+	    if(t>=10)
+	    c++;
+	}
+	cout<<c;
+	return 0;
+	
+}
