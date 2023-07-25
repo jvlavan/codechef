aec037d2d9281e5d95e4e905f25d38e4c48401ee
Lavan JV <70334120+jvlavan@users.noreply.github.com>
2023-07-25 19:01:05 +0530

    Create kitchentimings.cpp

---
+++ Day 01/kitchentimings.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	// your code goes here
+	int a;
+	cin>>a;
+	int b,c,d;
+	while(a--){
+	    cin>>b>>c;
+	    d=c-b;
+	    cout<<d<<endl;
+	
+	    
+	}
+	return 0;
+
+}
