64e6c0f81db3e7baff80866ab057965ac5dd7cbe
Lavan JV <70334120+jvlavan@users.noreply.github.com>
2023-07-25 18:22:01 +0530

    Create squats.cpp

---
+++ Day 01/squats.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	int a;
+	while(t--){
+	    cin>>a;
+	    cout<<(a*15)<<endl;
+	}
+	return 0;
+}
