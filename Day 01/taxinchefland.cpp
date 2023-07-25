fa4522b7cf676197f2a4bbd9e2be966dc4faff2f
Lavan JV <70334120+jvlavan@users.noreply.github.com>
2023-07-25 19:03:11 +0530

    Create taxinchefland.cpp

---
+++ Day 01/taxinchefland.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a;
+	cin>>a;
+	int b;
+	while(a--){
+	    cin>>b;
+	    if(b>100)
+	    cout<<(b-10)<<endl;
+	    else
+	    cout<<b<<endl;
+	
+	    
+	}
+	return 0;
+}
