45f67eff53a5df1e901314ba00c580b680dda0c7
Lavan JV <70334120+jvlavan@users.noreply.github.com>
2023-07-25 18:52:45 +0530

    Create Determinethescore.cpp

---
+++ Day 01/Determinethescore.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a;
+	cin>>a;
+	int b,c;
+	while(a--){
+	    cin>>b>>c;
+	    cout<<(b/10)*c<<endl;
+	    
+	    
+	}
+	return 0;
+}
