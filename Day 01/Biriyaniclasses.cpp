ec5c36f8cf5c6b6c3d850005125736c1672880e1
Lavan JV <70334120+jvlavan@users.noreply.github.com>
2023-07-25 18:34:54 +0530

    Create Biriyaniclasses.cpp

---
+++ Day 01/Biriyaniclasses.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a;
+	cin>>a;
+	int b,c;
+	while(a--){
+	    cin>>b>>c;
+	    cout<<b*c<<endl;
+	    
+	}
+	return 0;
+}
